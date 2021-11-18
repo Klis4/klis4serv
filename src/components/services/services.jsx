fbaf41d82b29241bb83f28944f108c643b0cbd2c
Klis4 <kleshcenok.ilya@gmail.com>
2021-11-18 16:14:29 +0300

    feat: add services component

---
+++ src/components/services/services.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './services.css';
+
+function Services () {
+  return (
+    <div className="container">
+      <section className="services">
+        <div className="our-services" id="articles">
+          <h2 className="our-services-header">Наши услуги</h2> 
+          <div className="slider-images">
+            <div className="slide">
+              <img src="../../assets/icons/pc.svg" alt="Pic." className="slide-image"></img>
+              <h4 className="slide-header">Ремонт компьютера</h4>  
+            </div>
+            <div className="slide">
+              <img src="../../assets/icons/phone.svg" alt="Pic." className="slide-image"></img>
+              <h4 className="slide-header">Ремонт телефона</h4>  
+            </div>
+            <div className="slide">
+              <img src="../../assets/icons/laptop.svg" alt="Pic." className="slide-image"></img>
+              <h4 className="slide-header">Ремонт ноутбука</h4>  
+            </div>
+          </div>
+        </div>
+
+        <div className="get-notified">
+          <h3 className="get-notified-header">Оставить заявку на консультацию</h3>
+          <div className="send-notification">
+            <input type="text" placeholder="Фамилия и имя" className="name-field"></input>
+            <input type="text" placeholder="+375 (XX) XXX-XX-XX" className="phone-field"></input>
+            <button type="submit" className="send-btn">Отправить</button>
+          </div>
+        </div>
+      
+      </section>
+    </div>  
+
+  );
+}
+
+export default Services();
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header/header.jsx';
 import Main from './components/main/main.jsx';
+import Services from './components/services/services.jsx'
 import './style.css';
 
 console.log(Header);
 ReactDOM.render(
   [
     Header, 
-    Main
+    Main, 
+    Services,
   ],
   document.querySelector('#root'),
 );
